67e13040b1e53f25d3ceac0c6ad60e96a4f997e8
Jérôme Alves <j.alves@me.com>
2018-11-28 19:08:45 +0100

    Require element to be Equatable instead of Hashable

--- Sources/Operators/filterIn.swift
+++ Sources/Operators/filterIn.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 extension Sequence {
-    public func filter<S: Sequence, T: Hashable>(where attribute: KeyPath<Element, T>, in values: S) -> [Element] where S.Element == T {
+    public func filter<S: Sequence, T: Equatable>(where attribute: KeyPath<Element, T>, in values: S) -> [Element] where S.Element == T {
         return filter { values.contains($0[keyPath: attribute]) }
     }
 
-    public func filter<S: Sequence, T: Hashable>(where values: S, contains attribute: KeyPath<Element, T>) -> [Element] where S.Element == T {
+    public func filter<S: Sequence, T: Equatable>(where values: S, contains attribute: KeyPath<Element, T>) -> [Element] where S.Element == T {
         return filter { values.contains($0[keyPath: attribute]) }
     }
 }
